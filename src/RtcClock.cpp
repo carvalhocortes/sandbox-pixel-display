51fad25f591f1ccbe11ea10a9b2456a30f93b5db
CarvalhoCortes <carvalhocortes@gmail.com>
2026-08-08 18:06:54 -0300

    fix: diagnosticar inicializacao I2C do RTC

--- src/RtcClock.cpp
+++ src/RtcClock.cpp
@@ -1,10 +1,26 @@
 #include "RtcClock.h"
 
+#include <Arduino.h>
 #include <Wire.h>
 
 bool RtcClock::begin(uint8_t sdaPin, uint8_t sclPin) {
-  Wire.begin(sdaPin, sclPin);
-  return rtc.begin();
+  for (uint8_t attempt = 1; attempt <= 3; attempt++) {
+    Wire.begin(sdaPin, sclPin);
+    Wire.setClock(100000);
+    delay(20);
+
+    Wire.beginTransmission(0x68);
+    const uint8_t addressStatus = Wire.endTransmission();
+    Serial.printf("RTC I2C tentativa %u: status=%u\n", attempt, addressStatus);
+
+    if (addressStatus == 0 && rtc.begin()) {
+      return true;
+    }
+
+    delay(100);
+  }
+
+  return false;
 }
 
 void RtcClock::synchronizeToBuildTime() {
