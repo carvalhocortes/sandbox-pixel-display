0d597f009e99c3a8f3b7f057206f9566d93fb3c3
CarvalhoCortes <carvalhocortes@gmail.com>
2026-08-08 16:33:38 -0300

    feat: exibir horario do RTC no serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,47 @@
 #include <Arduino.h>
+#include <RTClib.h>
+#include <Wire.h>
 
-unsigned long counter = 0;
+constexpr uint8_t SDA_PIN = D2;
+constexpr uint8_t SCL_PIN = D1;
+
+RTC_DS1307 rtc;
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
   Serial.println();
-  Serial.println("Serial counter test");
+  Serial.println("RTC serial test");
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  if (!rtc.begin()) {
+    Serial.println("RTC nao encontrado");
+    while (true) {
+      delay(1000);
+    }
+  }
+
+  // Sincroniza o RTC com o horario usado na compilacao deste firmware.
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void loop() {
-  Serial.println(++counter);
-  delay(500);
+  DateTime now = rtc.now();
+
+  char timestamp[24];
+  snprintf(
+      timestamp,
+      sizeof(timestamp),
+      "%02d/%02d/%04d %02d:%02d:%02d",
+      now.day(),
+      now.month(),
+      now.year(),
+      now.hour(),
+      now.minute(),
+      now.second());
+
+  Serial.println(timestamp);
+  delay(1000);
 }
